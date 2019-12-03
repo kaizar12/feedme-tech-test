9ac5b6d2c676a46612fdbc809c764f30136ce01c
Kaizar Laxmidhar <kaizar@Kaizars-MacBook-Pro.local>
2019-12-03 15:29:44 +0000

    implement open and close provider server connection and update unit tests

--- src/main/java/com/starsgroup/service/FeedProprietaryDataService.java
+++ src/main/java/com/starsgroup/service/FeedProprietaryDataService.java
@@ -1,26 +1,47 @@
 package com.starsgroup.service;
 
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 
 @Service
 @Setter
 public class FeedProprietaryDataService {
 
-    public void receiveData() {
+    private Socket clientSocket;
+    private BufferedReader in;
+    @Value("${provider.service.host}")
+    private String providerHost;
+    @Value("${provider.service.port}")
+    private int providerPort;
 
+    public void receiveData() throws IOException {
+        try {
+            startConnection(providerHost, providerPort);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            stopConnection();
+        }
     }
 
     public void abortFeedProcess() {
 
     }
 
-    private void startConnection() {
-
+    private void startConnection(String ip, int port) throws IOException {
+        if (clientSocket == null) clientSocket = new Socket(ip, port);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
 
-    private void stopConnection() {
-
+    private void stopConnection() throws IOException {
+        if (in != null) in.close();
+        clientSocket.close();
     }
 
 }
--- src/test/java/com/starsgroup/service/FeedProprietaryDataServiceTest.java
+++ src/test/java/com/starsgroup/service/FeedProprietaryDataServiceTest.java
@@ -3,20 +3,32 @@ package com.starsgroup.service;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FeedProprietaryDataServiceTest {
 
     private FeedProprietaryDataService feedProprietaryDataService;
 
+    @Mock
+    Socket socketMocked;
+
+    @Mock
+    InputStream inputStreamMocked;
+
     @Before
     public void setUp() throws IOException {
         feedProprietaryDataService = new FeedProprietaryDataService();
+        feedProprietaryDataService.setClientSocket(socketMocked);
+        when(socketMocked.getInputStream()).thenReturn(inputStreamMocked);
     }
 
     @Test
